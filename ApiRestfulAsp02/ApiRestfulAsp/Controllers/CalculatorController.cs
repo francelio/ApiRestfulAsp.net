6f29fff94cf989679e8ce354a7d190ad60b29296
Francelio Alencar <francelio.si@gmail.com>
2019-03-17 20:09:44 -0300

    implementing Rest calculator

--- ApiRestfulAsp02/ApiRestfulAsp/Controllers/CalculatorController.cs
+++ ApiRestfulAsp02/ApiRestfulAsp/Controllers/CalculatorController.cs
@@ -6,41 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiRestfulAsp.Controllers
 {
+
 	[Route("api/[controller]")]
-	[ApiController]
-	public class CalculatorController : ControllerBase
+	public class CalculatorController : Controller
 	{
-		// GET api/values/5
-		[HttpGet("{firstNumber}/{secondNumber}")]
-		public IActionResult Mult(string firstNumber, string secondNumber)
+
+		// GET api/values/sum/5/5
+		[HttpGet("sum/{firstNumber}/{secondNumber}")]
+		public IActionResult Sum(string firstNumber, string secondNumber)
 		{
 			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
 			{
-				var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+				var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
 				return Ok(sum.ToString());
 			}
 
 			return BadRequest("Invalid Input");
-
 		}
 
-		// GET api/values/5
-		[HttpGet("{firstNumber}/{secondNumber}")]
-		public IActionResult Div(string firstNumber, string secondNumber)
-		{
-			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
-			{
-				var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
-				return Ok(sum.ToString());
-			}
-
-			return BadRequest("Invalid Input");
-
-		}
-
-		// GET api/values/5
-		[HttpGet("{firstNumber}/{secondNumber}")]
-		public IActionResult Sub(string firstNumber, string secondNumber)
+		// GET api/values/subtraction/5/5
+		[HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+		public IActionResult Subtraction(string firstNumber, string secondNumber)
 		{
 			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
 			{
@@ -49,22 +35,54 @@ namespace ApiRestfulAsp.Controllers
 			}
 
 			return BadRequest("Invalid Input");
-
 		}
 
-
-
-		// GET api/values/5
-		[HttpGet("{firstNumber}/{secondNumber}")]
-		public IActionResult Sum(string firstNumber, string secondNumber)
+		// GET api/values/division/5/5
+		[HttpGet("division/{firstNumber}/{secondNumber}")]
+		public IActionResult Division(string firstNumber, string secondNumber)
 		{
-			if (IsNumeric(firstNumber) && IsNumeric(secondNumber)) {
-				var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-				return Ok(sum.ToString());
+			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+			{
+				var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+				return Ok(division.ToString());
 			}
+			return Ok("Invalid Input");
+		}
 
-			return BadRequest("Invalid Input");
-			
+		// GET api/values/multiplication/5/5
+		[HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+		public IActionResult Multiplication(string firstNumber, string secondNumber)
+		{
+			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+			{
+				var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+				return Ok(multiplication.ToString());
+			}
+			return Ok("Invalid Input");
+		}
+
+		// GET api/values/mean/5/5
+		[HttpGet("mean/{firstNumber}/{secondNumber}")]
+		public IActionResult Mean(string firstNumber, string secondNumber)
+		{
+			if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+			{
+				var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+				return Ok(mean.ToString());
+			}
+			return Ok("Invalid Input");
+		}
+
+		// GET api/values/square-root/5
+		[HttpGet("square-root/{number}")]
+		public IActionResult SquareRoot(string number)
+		{
+			if (IsNumeric(number))
+			{
+				var squareRoot = Math.Sqrt((double)ConvertToDecimal(number));
+				return Ok(squareRoot.ToString());
+			}
+			return Ok("Invalid Input");
 		}
 
 		private decimal ConvertToDecimal(string number)
@@ -75,7 +93,6 @@ namespace ApiRestfulAsp.Controllers
 				return decimalValue;
 			}
 			return 0;
-
 		}
 
 		private bool IsNumeric(string strNumber)
