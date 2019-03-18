1d907dcbd81a75b9542c68483ceba3f4dc6a837b
Francelio Alencar <francelio.si@gmail.com>
2019-03-18 09:26:27 -0300

    Create database

---
+++ ApiRestfulAsp04/ApiRestfulAsp/Controllers/PersonsController.cs
@@ -0,0 +1,60 @@
+﻿using ApiRestfulAsp.Model;
+using ApiRestfulAsp.Services.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRestfulAsp.Controllers
+{
+
+	[Route("api/[controller]")]
+	public class PersonsController : Controller
+	{
+		private IPersonService _personService;
+
+		public PersonsController(IPersonService personService)
+		{
+			_personService = personService;
+		}
+
+		// GET api/values
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok(_personService.FindAll());
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public IActionResult Get(long id)
+		{
+			var person = _personService.FindById(id);
+			if (person == null) return NotFound();
+
+			return Ok(person);
+		}
+
+		// POST api/values
+		[HttpPost]
+		public IActionResult Post([FromBody] Person person)
+		{
+			if (person == null) return BadRequest();
+			return new ObjectResult(_personService.Create(person));
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public IActionResult Put([FromBody] Person person)
+		{
+			if (person == null) return BadRequest();
+			return new ObjectResult(_personService.Update(person));
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public IActionResult Delete(long id)
+		{
+			_personService.Delete(id);
+			return NoContent();
+		}
+	}
+
+}
