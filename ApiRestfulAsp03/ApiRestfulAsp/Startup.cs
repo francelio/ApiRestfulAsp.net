cbdd99dfc029a69c7140c37999c95697ede356d1
Francelio Alencar <francelio.si@gmail.com>
2019-03-17 21:17:52 -0300

    implementing integration Controller in services

--- ApiRestfulAsp03/ApiRestfulAsp/Startup.cs
+++ ApiRestfulAsp03/ApiRestfulAsp/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiRestfulAsp.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,9 @@ namespace ApiRestfulAsp
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			//Dependency Injection
+			services.AddScoped<IPersonService, PersonServiceImpl>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
